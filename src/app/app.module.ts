f8571cb80b455051f80fffdd4f8ce088b0dbe45b
Williams Caballero Araya <knightex22@gmail.com>
2022-01-02 21:42:30 -0300

    Agregar componentes y servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEmpleadosComponent,
+    CrearEmpleadoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/crear-empleado/crear-empleado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-empleado',
+  templateUrl: './crear-empleado.component.html',
+  styleUrls: ['./crear-empleado.component.scss']
+})
+export class CrearEmpleadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
